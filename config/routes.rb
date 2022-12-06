e1e976e544d523ed802feed0c4dea1cd1e46dbcd
mazacon <mazacon@protonmail.com>
2022-12-05 23:40:48 -0600

    create controllers for users sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
