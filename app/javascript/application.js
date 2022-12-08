8f46a415f3886399acd2a6816e11afa48e471a66
mazacon <mazacon@protonmail.com>
2022-12-08 08:54:50 -0600

    beautify landing page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+if (document.getElementById('typewriter')) {
+    var tw = document.getElementById('typewriter');
+
+    var typewriter = new Typewriter(tw, {
+      loop: true,
+      delay: 70,
+    });
+    
+    typewriter
+      .pauseFor(1500)
+      .typeString('An <strong>RSS reader web application</strong>')
+      .pauseFor(3000)
+      .deleteChars(29)
+      .pauseFor(200)
+      .typeString('made with <strong>Ruby on Rails 7</strong>')
+      .pauseFor(3000)
+      .deleteChars(25)
+      .pauseFor(200)
+      .typeString('by Maz for <strong>Backstitch.io</strong>')
+      .pauseFor(3000)
+      .deleteChars(24)
+      .pauseFor(200)
+      .start();
+}
