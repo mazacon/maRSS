f873e8a7f1d0bc7f503667c5795620997bb6d653
mazacon <mazacon@protonmail.com>
2022-12-12 08:18:52 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,14 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_231551) do
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
-  create_table "rss_links", force: :cascade do |t|
-    t.text "url"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_rss_links_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -35,5 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_231551) do
   end
 
   add_foreign_key "feeds", "users"
-  add_foreign_key "rss_links", "users"
 end
